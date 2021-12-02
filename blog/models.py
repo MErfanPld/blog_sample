6be55579dfab3f50b667213cebb25b63f8ec2d20
Mohammad Erfan Pouladzadeh <m.erfanpouladzadeh@yahoo.com>
2021-12-02 18:14:50 +0330

    Create author

---
+++ blog/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from extensions.utils import jalali_converter
+
+
+# my managers
+class ArticleManager(models.Manager):
+    def published(self):
+        return self.filter(status='p')
+
+
+class CategoryManager(models.Manager):
+    def active(self):
+        return self.filter(status=True)
+
+
+# Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, verbose_name="عنوان")
+    slug = models.SlugField(max_length=200, unique=True, verbose_name="ادرس")
+    status = models.BooleanField(
+        default=True, verbose_name="ایا نمایش داده شود؟")
+    position = models.IntegerField(default=0, verbose_name="پوزیشن")
+
+    class Meta:
+        verbose_name = "دسته بندی"
+        verbose_name_plural = "دسته بندی ها"
+        ordering = ['position']
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'پیش نویس'),
+        ('p', 'منتشر شده'),
+    )
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='articles', verbose_name="نویسنده")
+    title = models.CharField(max_length=200, verbose_name="عنوان")
+    slug = models.SlugField(max_length=200, unique=True, verbose_name="ادرس")
+    category = models.ManyToManyField(
+        Category, verbose_name="دسته بندی", related_name="articles")
+    description = models.TextField(verbose_name="توضیحات")
+    thumbnail = models.ImageField(upload_to='images', verbose_name="تصویر")
+    publish = models.DateTimeField(
+        default=timezone.now, verbose_name="زمان انتشار")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=1, choices=STATUS_CHOICES, verbose_name="وضعیت")
+
+    class Meta:
+        verbose_name = "مقاله"
+        verbose_name_plural = "مقالات"
+        ordering = ['-publish']
+
+    def __str__(self):
+        return self.title
+
+    def jpublish(self):
+        return jalali_converter(self.publish)
+    jpublish.short_description = "زمان انتشار"
+
+    def category_to_str(self):
+        return "، ".join([category.title for category in self.category.all()])
+    category_to_str.short_description = "دسته‌بندی"
+
+    def category_published(self):
+        return self.category.filter(status=True)
+
+    objects = ArticleManager()
---
+++ blog/views.py
@@ -0,0 +1,56 @@
+from django.core.paginator import Paginator
+from django.db.models.query import QuerySet
+from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+from .models import Article, Category
+
+# Create your views here.
+
+
+class ArticleList(ListView):
+    queryset = Article.objects.published()
+    paginate_by = 3
+    # context_object_name = "articles"
+    # model = Article
+    # template_name = 'blog/home.html'
+
+
+class ArticleDetail(DetailView):
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        return get_object_or_404(Article.objects.published(), slug=slug)
+
+
+class CategoryList(ListView):
+    paginate_by = 2
+    template_name = 'blog/category_list.html'
+
+    def get_queryset(self):
+        global category
+        slug = self.kwargs.get('slug')
+        category = get_object_or_404(Category.objects.all(), slug=slug)
+        return category.articles.published()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = category
+        return context
+
+
+class AuthorList(ListView):
+    paginate_by = 5
+    template_name = 'blog/author_list.html'
+
+    def get_queryset(self):
+        global author
+        username = self.kwargs.get('username')
+        author = get_object_or_404(User, username=username)
+        return author.articles.published()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['author'] = author
+        return context
+
+
