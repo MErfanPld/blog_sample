3cf631d7d0cb6427a50c3a10ed5367eda000d07b
Mohammad Erfan Pouladzadeh <m.erfanpouladzadeh@yahoo.com>
2021-12-06 17:43:51 +0330

    Create Password reset

--- config/settings.py
+++ config/settings.py
@@ -145,3 +145,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 AUTH_USER_MODEL = 'account.User'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('blog.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
 ]
